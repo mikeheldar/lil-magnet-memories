13e7ea20ec5414d5037517ffabd770b82d8628c3
mikeheldar <michael.helmandarley@gmail.com>
2023-02-13 22:06:50 -0500

    added access token and password to serverLogin function and called and got jwt from server

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <div class="column items-center" style="height: 150px">
+    <div class="column items-center">
       <div class="col">
         <GoogleLogin :callback="googleAuth" style="width: 248px" />
       </div>
@@ -11,6 +11,18 @@
           icon="fa-facebook-f"
           label="Login with Facebook"
           @click="helloAuth('facebook')"
+          style="width: 248px"
+        >
+        </q-btn>
+      </div>
+      <div class="col q-pt-lg">
+        <q-input filled v-model="userEmail" label="email address (login)" />
+        <q-input filled v-model="userPassword" label="password" />
+        <q-btn
+          color="primary"
+          glossy
+          label="login"
+          @click="serverLogin(userEmail, '')"
           style="width: 248px"
         >
         </q-btn>
@@ -43,7 +55,8 @@ export default {
       const userData = decodeCredential(response.credential);
       console.log('Handle the userData', userData);
       console.log('Handle the email', userData.email);
-      this.serverLogin(userData.email);
+      console.log('Handle the token', userData.jti);
+      this.serverLogin(userData.email, userData.jti);
     },
     helloAuth(network) {
       this.$hello(network)
@@ -54,21 +67,27 @@ export default {
         })
         .then((res) => {
           console.log(res);
+          console.log('saving access_token: ' + res.authResponse.access_token);
+          const access_token = res.authResponse.access_token;
+
           this.$hello(network)
             .api('me')
             .then((res) => {
               console.log(res.email);
-              this.serverLogin(res.email);
+              console.log('facebook me res: ' + JSON.stringify(res));
+              this.serverLogin(res.email, access_token);
             });
         });
     },
-    serverLogin(userEmail) {
+    serverLogin(userEmail, userToken) {
       console.log('In serverLogin, userEmail', userEmail);
 
       const payload = {
         email: userEmail,
+        token: userToken,
+        password: this.userPassword,
       };
-
+      console.log('userToken', JSON.stringify(userToken));
       console.log('payload to send: ', payload);
       this.$api
         .post('/api/login', payload)
@@ -80,6 +99,11 @@ export default {
           console.log('Error: ', err);
         });
     },
+  },
+  data() {
+    return {
+      userPassword: '',
+    };
   },
 };
 </script>
