2fc90dfbcb88100c547f4630dd301abed1c57781
mikeheldar <michael.helmandarley@gmail.com>
2025-12-31 11:54:45 -0500

    Increase Custom Photo Magnets breakpoint by another 100px to prevent About overlap
    - Increased Custom Photo Magnets breakpoint from 1400px to 1500px (additional 100px)
    - Also increased drawer-open breakpoint from 1700px to 1800px
    - Ensures Custom Photo Magnets menu drops off even earlier to prevent full overlap with About button
    - Provides more buffer space to maintain clean header layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1612,7 +1612,7 @@ export default {
 }
 
 // Hide Custom Photo Magnets last (leftmost menu) - hide well before About button to prevent overlap
-@media (max-width: 1400px) {
+@media (max-width: 1500px) {
   .shop-header-dropdowns .shop-header-btn-custom {
     display: none !important;
   }
@@ -1641,7 +1641,7 @@ export default {
   }
 
   // Hide Custom Photo Magnets earlier when drawer is open
-  @media (max-width: 1700px) {
+  @media (max-width: 1800px) {
     .shop-header-dropdowns .shop-header-btn-custom {
       display: none !important;
     }
